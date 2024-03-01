731a7f50e1000d29a221873065e4b8bd054e9045
daeyeon <kdy4410121@naver.com>
2024-03-01 14:38:52 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,37 +1,31 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main{
-
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String []args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-
-        int[] coin = new int[N];
-
-        for(int i = 0; i < N; i++) {
-            coin[i] = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int spe = Integer.parseInt(st.nextToken());
+        int total = Integer.parseInt(st.nextToken());
+        int[] kind = new int[spe];
+        for(int i = 0; i < spe; i++){
+            kind[i] = Integer.parseInt(br.readLine());
         }
-
         int count = 0;
-
-        for(int i = N - 1; i >= 0; i--) {
-
-            // 현재 동전의 가치가 K보다 작거나 같아야지 구성가능하다.
-            if(coin[i] <= K) {
-                // 현재 가치의 동전으로 구성할 수 있는 개수를 더해준다.
-                count += (K / coin[i]);
-                K = K % coin[i];
+        int result = spe-1;
+        int c = 0;
+        while(total != 0){
+            if(total >= kind[result]) {
+                count += total / kind[result];
+                c = total / kind[result];
+                total -= c *  kind[result];
             }
+            else
+                result--;
+
         }
         System.out.println(count);
     }
-
 }
