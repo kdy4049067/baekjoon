7fb024cad68778d6c70d0015ca04ee1c38a37ecc
daeyeon <kdy4410121@naver.com>
2024-03-12 16:39:38 +0900

    [Silver IV] Title: 설탕 배달, Time: 120 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,39 +1,29 @@
+import java.awt.image.DataBufferDouble;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	
-	public static void main(String[] args)throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int kg = Integer.parseInt(br.readLine());
-		int kg5 = kg / 5;
-		int kg3 = 0;
-		
-		for(int i = kg5; i > 0; i--) {
-			if((kg - i * 5) % 3 == 0) {
-				kg3 = (kg - i * 5) / 3;
-				System.out.println(kg3+i);
-				return;
-			}
-			if(i == 1) {
-				if(kg % 3 == 0) {
-					System.out.println(kg / 3);
-					return;
-				}
-			}
-		}
-		if(kg <5) {
-			if(kg == 3) {
-				System.out.println(1);
-				return;
-			}
-			System.out.println(-1);
-            return;
-		}
-		System.out.println(-1);
-		
-	}
+    static int[] dp;
+    public static void main(String[]args) throws IOException{
+        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+        int kg = Integer.parseInt(br.readLine());
+        dp = new int[kg+1];
+        System.out.println(sugar(kg));
 
+    }
+    public static int sugar(int kg){
+        if(kg == 4 || kg == 7)
+            return -1;
+        if(kg % 5 == 0) {
+            return kg / 5;
+        }
+        else if(kg % 5 == 2 || kg % 5 == 4)
+            return kg / 5 + 2;
+        else if(kg % 5 == 1 || kg % 5 == 3)
+            return kg / 5 + 1;
+
+        else
+            return 0;
+    }
 }
