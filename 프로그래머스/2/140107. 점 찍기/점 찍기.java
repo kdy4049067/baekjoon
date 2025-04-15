33ecac45b83816e6a4d9777474ad3c3d47358126
daeyeon <kdy4410121@naver.com>
2025-04-15 11:07:53 +0900

    [level 2] Title: 점 찍기, Time: 16.46 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int k, int d) {
+        long answer = 0;
+        
+        for(int i = 0; i <= d; i+=k){
+            long b = (long)Math.sqrt(Math.pow(d,2) - Math.pow(i,2));
+            answer += (long)b / k + 1;
+        }
+        
+        return answer;
+    }
+}
