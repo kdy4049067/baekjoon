b2f9e881f225cc12b0505eb2b5f243d7c4d78520
daeyeon <kdy4410121@naver.com>
2026-01-27 23:08:33 +0900

    [Platinum V] Title: 책 페이지, Time: 188 ms, Memory: 18316 KB -BaekjoonHub

---
+++ 백준/Platinum/1019. 책 페이지/책 페이지.java
@@ -0,0 +1,55 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    private static int[] cnt;
+    private static int digit = 1;
+    private static int start = 1;
+    private static int end;
+
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+
+        digit = 1;
+        start = 1;
+        end = sc.nextInt();
+
+        cnt = new int[10];
+
+        while(start <= end) {
+            while(start % 10 != 0 && start <= end) {
+                counting(start, digit);
+                start++;
+            }
+
+            while(end % 10 != 9 && start <= end) {
+                counting(end, digit);
+                end--;
+            }
+
+            if(start > end) break;
+
+            start /= 10;
+            end /= 10;
+
+            for(int i = 0 ; i < 10 ; ++i) {
+                cnt[i] += (end - start + 1) * digit;
+            }
+
+            digit *= 10;
+        }
+
+        for(long i : cnt) {
+            System.out.print(i + " ");
+        }
+    }
+
+    private static void counting(int num, int digit) {
+        while(num > 0) {
+            cnt[num % 10] += digit;
+            num /= 10;
+        }
+    }
+
+}
