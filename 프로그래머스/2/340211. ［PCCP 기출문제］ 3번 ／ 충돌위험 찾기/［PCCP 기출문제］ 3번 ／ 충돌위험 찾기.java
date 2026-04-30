4c52801920750ee6964b954d79e4ee5d36b89192
daeyeon <kdy4410121@naver.com>
2026-04-30 23:39:27 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 491.72 ms, Memory: 192 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,84 +1,59 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
-
-    static Queue<int[]>[] memory;
-    static int size;
-    static int answer;
-
     public int solution(int[][] points, int[][] routes) {
-        size = routes.length;
-        memory = new LinkedList[size];
-        for(int i = 0; i < size; i++){
-            memory[i] = new LinkedList<>();
+        int answer = 0;
+        int robotCount = routes.length;
+        
+        // 1. 각 로봇의 시간대별 위치를 저장할 리스트 (Queue의 배열)
+        // robots[i] 에는 i번 로봇이 시간대별로 밟는 좌표들이 순서대로 들어감
+        Queue<int[]>[] robots = new LinkedList[robotCount];
+        
+        int maxTime = 0;
+        for (int i = 0; i < robotCount; i++) {
+            robots[i] = new LinkedList<>();
+            int[] current = points[routes[i][0] - 1];
+            robots[i].add(new int[]{current[0], current[1]}); // 시작점 추가 (0초)
+            
+            for (int j = 1; j < routes[i].length; j++) {
+                int[] next = points[routes[i][j] - 1];
+                int curR = current[0];
+                int curC = current[1];
+                int nextR = next[0];
+                int nextC = next[1];
+                
+                // R 좌표 먼저 이동
+                while (curR != nextR) {
+                    curR += (nextR > curR) ? 1 : -1;
+                    robots[i].add(new int[]{curR, curC});
+                }
+                // C 좌표 이동
+                while (curC != nextC) {
+                    curC += (nextC > curC) ? 1 : -1;
+                    robots[i].add(new int[]{curR, curC});
+                }
+                current = next; // 다음 목적지로 갱신
+            }
+            maxTime = Math.max(maxTime, robots[i].size());
         }
-        memo(points, routes);
-        crash();
+
+        // 2. 시간대별(0초 ~ 최대시간) 충돌 체크
+        for (int t = 0; t < maxTime; t++) {
+            Map<String, Integer> map = new HashMap<>();
+            for (int i = 0; i < robotCount; i++) {
+                // 해당 시간에 로봇이 아직 운행 중이라면
+                if (!robots[i].isEmpty()) {
+                    int[] pos = robots[i].poll();
+                    String key = pos[0] + "," + pos[1];
+                    map.put(key, map.getOrDefault(key, 0) + 1);
+                }
+            }
+            
+            for (int count : map.values()) {
+                if (count >= 2) answer++;
+            }
+        }
+        
         return answer;
-    }
-
-    public static void crash(){
-        int count = 0;
-        while(count != size){
-            int map[][] = new int[101][101];
-            count = 0;
-            for(int i = 0; i < size; i++){
-                if(memory[i].isEmpty()){
-                    count++;
-                    continue;
-                }
-                int [] temp = memory[i].poll();
-                map[temp[0]][temp[1]]++;
-            }
-            for(int i = 0; i < 101; i++){
-                for(int j = 0; j < 101; j++){
-                    if(map[i][j] > 1) answer++;
-                }
-            }
-        }
-    }
-
-    public static void memo(int [][] points, int [][] routes){
-        for(int i = 0; i < size; i++){
-            int [] point = points[routes[i][0] - 1];
-            int x = point[0];
-            int y = point[1];
-            memory[i].add(new int[]{x,y});
-            for(int j = 1; j < routes[0].length; j++){
-                int [] np = points[routes[i][j] - 1];
-                int nx = np[0];
-                int ny = np[1];
-
-                int xp = nx - x;
-                int yp = ny - y;
-
-                while(xp != 0){
-                    if(xp > 0){
-                        xp--;
-                        x++;
-                        memory[i].add(new int[]{x,y});
-                    }
-                    else{
-                        xp++;
-                        x--;
-                        memory[i].add(new int[]{x,y});
-                    }
-                }
-                while(yp != 0){
-                    if(yp > 0){
-                        yp--;
-                        y++;
-                        memory[i].add(new int[]{x, y});
-                    }
-                    else{
-                        yp++;
-                        y--;
-                        memory[i].add(new int[]{x, y});
-                    }
-                }
-            }
-
-        }
     }
 }
