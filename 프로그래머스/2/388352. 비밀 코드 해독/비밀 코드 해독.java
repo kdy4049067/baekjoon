1ac8c671817c7be7c1c893944a1c0a530facd2c1
daeyeon <kdy4410121@naver.com>
2026-05-07 11:43:20 +0900

    [level 2] Title: 비밀 코드 해독, Time: 12.36 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -1,37 +1,42 @@
 class Solution {
     
-    private static int Q_SIZE = 5;
-    private static boolean[] visited;
-    private static int result = 0;
+    private int[] candidate;
+    private int answer = 0;
     
     public int solution(int n, int[][] q, int[] ans) {
-        visited = new boolean[n+1];
-        comb(n, q, ans, 1, 0);
+        candidate = new int[5];
+        dfs(n, 1, 0, q, ans);
         
-        return result;
+        return answer;
     }
     
-    private void comb(int n, int[][] q, int[] ans, int index, int depth){
+    private void dfs(int n, int start, int depth, int[][] q, int[] ans){
         if(depth == 5){
-            for(int i = 0; i < q.length; i++){
-                int count = 0;
-                
-                for(int j = 0; j < Q_SIZE; j++){
-                    if(count > ans[i]) break;
-                    if(visited[q[i][j]]) count++;
-                }
-                if(ans[i] != count) return;
+            if(simulate(q, ans)){
+                answer++;
             }
-            
-            result++;
             return;
         }
-    
         
-        for(int i = index; i <= n; i++){
-            visited[i] = true;
-            comb(n, q, ans, i+1, depth+1);
-            visited[i] = false;
+        for(int i = start; i <= n; i++){
+            candidate[depth] = i;
+            dfs(n, i+1, depth+1, q, ans);
         }
+    }
+    
+    private boolean simulate(int[][] q, int[] ans){
+        for(int i = 0; i < q.length; i++){
+            int[] qq = q[i];
+            int count = 0;
+            for(int j = 0; j < qq.length; j++){
+                for(int k = 0; k < qq.length; k++){
+                    if(qq[j] == candidate[k]) count++;
+                }
+            }
+            
+            if(ans[i] != count) return false;
+        }
+        
+        return true;
     }
 }
